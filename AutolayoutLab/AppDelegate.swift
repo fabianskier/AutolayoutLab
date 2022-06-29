43198d172cc2669a2ab5848cde9e16b36236b603
Oscar Cristaldo <fabianskier@icloud.com>
2022-06-28 20:55:16 -0400

    CO-0001: Add safe area layout
    Signed-off-by: Oscar Cristaldo <fabianskier@icloud.com>

--- AutolayoutLab/AppDelegate.swift
+++ AutolayoutLab/AppDelegate.swift
@@ -16,7 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         window?.backgroundColor = .systemBackground
-        window?.rootViewController = ViewController()
+        // window?.rootViewController = AnchorViewController()
+        window?.rootViewController = SafeAreaViewController()
         
         return true
     }
