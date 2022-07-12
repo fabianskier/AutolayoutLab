fb372cbfc5c7367165e5335c80795f5025aca17f
Oscar Cristaldo <fabianskier@icloud.com>
2022-07-11 21:15:36 -0400

    CO-0010: Add views inside a stack view
    Signed-off-by: Oscar Cristaldo <fabianskier@icloud.com>

--- AutolayoutLab/CustomViewChallenge.swift
+++ AutolayoutLab/CustomViewChallenge.swift
@@ -9,28 +9,28 @@
 import UIKit
 
 class CustomViewChallenge: UIViewController {
-
+    
     let margin: CGFloat = 20
     let spacing: CGFloat = 32
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavigationBar()
         setupViews()
     }
-
+    
     func setupNavigationBar() {
         navigationItem.title = "Playback"
     }
-
+    
     /*
      
      Challenge here is to take the various controls and practice extracting components as subViews (UIViews).
-
+     
      Two good candidates for extraction are the:
-
-      - RowView (label and a switch)
-      - CrossfadeView (label, progressbar, and max min label)
+     
+     - RowView (label and a switch)
+     - CrossfadeView (label, progressbar, and max min label)
      
      ┌───────────────┐        ┌───────────────┐
      │               │        │               │
@@ -48,7 +48,33 @@ class CustomViewChallenge: UIViewController {
      See if you can extract those into their own custom UIViews and then lay those out.
      
      */
+    
+    /*
+     
+     Challenge: See if you can embed all these controls in a StackView like this.
+     
+     ┌────────StackView─────────┐
+     │                          │
+     │        offlineRow        │
+     │                          │
+     │      offlineSublabel     │
+     │                          │   axis = vertical
+     │       crossfadeView      │   distribution = fill
+     │                          │   alignment = fill
+     │        gaplessRow        │   spacing = 20
+     │                          │
+     │       hideSongsRow       │
+     │                          │
+     │     normalizationRow     │
+     │                          │
+     └──────────────────────────┘
+     
+     */
+    
     func setupViews() {
+        let stackView = makeStackView(withOrientation: .vertical)
+        stackView.spacing = 20
+        
         let offlineRow = RowView(title: "Offline", isOn: false)
         let offlineSublabel = makeSubLabel(withText: "When you go offline, you'll only be able to play the music and podcasts you've downloaded.")
         let crossfadeView = CrossfadeView()
@@ -61,38 +87,26 @@ class CustomViewChallenge: UIViewController {
         gaplessRow.translatesAutoresizingMaskIntoConstraints = false
         hideSongRow.translatesAutoresizingMaskIntoConstraints = false
         normalizationRow.translatesAutoresizingMaskIntoConstraints = false
-
-        view.addSubview(offlineRow)
-        view.addSubview(offlineSublabel)
-        view.addSubview(crossfadeView)
-        view.addSubview(gaplessRow)
-        view.addSubview(hideSongRow)
-        view.addSubview(normalizationRow)
-
-        offlineRow.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: margin).isActive = true
-        offlineRow.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: margin).isActive = true
-        offlineRow.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -margin).isActive = true
-
-        offlineSublabel.topAnchor.constraint(equalTo: offlineRow.bottomAnchor, constant: margin).isActive = true
-        offlineSublabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: margin).isActive = true
-        offlineSublabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -margin).isActive = true
-
-        crossfadeView.topAnchor.constraint(equalTo: offlineSublabel.bottomAnchor, constant: margin).isActive = true
-        crossfadeView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: margin).isActive = true
-        crossfadeView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -margin).isActive = true
         
-        gaplessRow.topAnchor.constraint(equalTo: crossfadeView.bottomAnchor, constant: margin).isActive = true
-        gaplessRow.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: margin).isActive = true
-        gaplessRow.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -margin).isActive = true
+        stackView.addArrangedSubview(offlineRow)
+        stackView.addArrangedSubview(offlineSublabel)
+        stackView.addArrangedSubview(crossfadeView)
+        stackView.addArrangedSubview(gaplessRow)
+        stackView.addArrangedSubview(hideSongRow)
+        stackView.addArrangedSubview(normalizationRow)
         
-        hideSongRow.topAnchor.constraint(equalTo: gaplessRow.bottomAnchor, constant: margin).isActive = true
-        hideSongRow.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: margin).isActive = true
-        hideSongRow.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -margin).isActive = true
+        view.addSubview(stackView)
         
-        normalizationRow.topAnchor.constraint(equalTo: hideSongRow.bottomAnchor, constant: margin).isActive = true
-        normalizationRow.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: margin).isActive = true
-        normalizationRow.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -margin).isActive = true
-
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: margin),
+            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: margin),
+            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -margin)
+        ])
+        
+        
+        // Padding and spacing
+        stackView.isLayoutMarginsRelativeArrangement = true
+        stackView.directionalLayoutMargins = NSDirectionalEdgeInsets(top: 4, leading: 4, bottom: 4, trailing: 4)
     }
 }
 
